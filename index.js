4ef7961716d995ca924465aecd3fa7a0716f7a3f
Abdullah Darsot <abdullahdarsot1@gmail.com>
2021-11-11 09:20:19 +0000

    First Big Commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// let submitForm = () => {
+//     document.getElementById('submit').style.color= 'green';
+// }
+let btn = document.getElementById('submit');
+
+// let submitForm = () => {
+//     btn.classList.remove("color1")
+//     btn.classList.add("color2")
+//   }
+  
+let myFunction = () => {
+    document.getElementById('change').innerHTML = 'Hello World';
+}
