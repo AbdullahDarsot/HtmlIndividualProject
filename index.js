b8d36b64bc948bb324d2cbe6e23ac953b3ec9338
Abdullah Darsot <abdullahdarsot1@gmail.com>
2021-11-11 10:07:32 +0000

    Partial Commit

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
-// let submitForm = () => {
-//     document.getElementById('submit').style.color= 'green';
-// }
+/*
+let submitForm = () => {
+     document.getElementById('submit').style.color= 'green';
+ }
 let btn = document.getElementById('submit');
 
-// let submitForm = () => {
-//     btn.classList.remove("color1")
-//     btn.classList.add("color2")
-//   }
+ let submitForm = () => {
+     btn.classList.remove("color1")
+     btn.classList.add("color2")
+   }
   
-let myFunction = () => {
-    document.getElementById('change').innerHTML = 'Hello World';
-}
+ let myFunction = () => {
+   document.getElementById('change').innerHTML = 'Submitted';
+ }
+*/
+
+//removes form once its been submitted
+function myFunction() {
+    var myobj = document.getElementById("form1");
+    myobj.remove();
+    alert("Submitted");
+  }
+
+
+
+
